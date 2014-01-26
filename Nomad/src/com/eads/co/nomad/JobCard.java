f452676fe7830931124cecfb373ba83d0314c79e
Bruniquel <bruniquel.nicolas@free.fr>
2014-01-26 17:41:07 +0100

    Ajout jobcard

--- Nomad/src/com/eads/co/nomad/JobCard.java
+++ Nomad/src/com/eads/co/nomad/JobCard.java
@@ -1,9 +1,13 @@
 package com.eads.co.nomad;
 
 import android.app.Activity;
+import android.os.Bundle;
 
 public class JobCard extends Activity{
 
-	
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.jobcard);
+    }
 	
 }
