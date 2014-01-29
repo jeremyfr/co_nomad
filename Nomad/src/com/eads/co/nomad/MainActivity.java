296a9ff6b9a7038169ded5f97ed4b21a9df38ee4
Jeremy Fricou <jeremyfricou@gmail.com>
2014-01-29 21:11:04 +0100

    [Connexion] Ajout de l'écran de connexion (pas de vérif d'ID)

--- Nomad/src/com/eads/co/nomad/MainActivity.java
+++ Nomad/src/com/eads/co/nomad/MainActivity.java
@@ -1,10 +1,14 @@
 package com.eads.co.nomad;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.TextView.OnEditorActionListener;
 
 public class MainActivity extends Activity {
 
@@ -12,22 +16,18 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
-        return true;
-    }
-    
-    public void toAMM(View view){
-        Intent intent = new Intent(this, AMM.class);
-        startActivity(intent);
+        /* Verification du mot de passe lors du clic sur 'Ok' du clavier */
+        ((EditText) findViewById(R.id.motDePasse)).setOnEditorActionListener(new OnEditorActionListener() {
+			@Override
+			public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+				toMenu(v);
+				return true;
+			}
+		});
     }
     
-    public void toJobCard(View view){
-        Intent intent = new Intent(this, JobCard.class);
+    public void toMenu(View view){
+        Intent intent = new Intent(this, MenuApp.class);
         startActivity(intent);
     }
 }
---
+++ Nomad/src/com/eads/co/nomad/MenuApp.java
@@ -0,0 +1,33 @@
+package com.eads.co.nomad;
+
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.view.Menu;
+import android.view.View;
+
+public class MenuApp extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.menu_app);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    public void toAMM(View view){
+        Intent intent = new Intent(this, AMM.class);
+        startActivity(intent);
+    }
+    
+    public void toJobCard(View view){
+        Intent intent = new Intent(this, JobCard.class);
+        startActivity(intent);
+    }
+}
