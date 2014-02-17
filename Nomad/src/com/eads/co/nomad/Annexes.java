ca139f6375e15ce2b1ee7fc40a6358f99e7d21a9
florian.lefebvre@eleve.enac.fr <Florian@Florian-N56VZ>
2014-02-17 19:42:17 +0100

    FullScreen / Fermeture de l'annexe possible.

--- Nomad/src/com/eads/co/nomad/Annexes.java
+++ Nomad/src/com/eads/co/nomad/Annexes.java
@@ -6,6 +6,7 @@ import android.view.DragEvent;
 import android.view.Menu;
 import android.view.MotionEvent;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.LinearLayout.LayoutParams;
@@ -14,13 +15,16 @@ import android.widget.Toast;
 
 public class Annexes extends Activity {
 
+	static int xmax = 1250; // 2500 pour la Nexus 10, à changer dans le layout
+							// annexes aussi.
+	static int xmin = xmax/5;
+
 	LinearLayout layout;
 	TextView textDocumentation;
 	LinearLayout annexLayout;
+	Button closeAnnexButton, fullScreenAnnexButton;
 	AnnexesState state = AnnexesState.NOT_DISPLAYED;
-	static int xmax = 1250; // 2500 pour la Nexus 10, à changer dans le layout
-							// annexes
-							// aussi.
+
 	int x = xmax;
 
 	private void setAnnexeX(int x) {
@@ -28,6 +32,10 @@ public class Annexes extends Activity {
 				LayoutParams.WRAP_CONTENT, 0f));
 		annexLayout.setLayoutParams(new LayoutParams(xmax - x,
 				LayoutParams.WRAP_CONTENT));
+	}
+
+	private void setAnnexeXAndX(int x) {
+		setAnnexeX(x);
 		this.x = x;
 	}
 
@@ -38,7 +46,11 @@ public class Annexes extends Activity {
 		layout = (LinearLayout) findViewById(R.id.linearLayout1);
 		textDocumentation = (TextView) findViewById(R.id.textDocumentation);
 		annexLayout = (LinearLayout) findViewById(R.id.annexLayout);
+		closeAnnexButton = (Button) findViewById(R.id.closeAnnexButton);
+		fullScreenAnnexButton = (Button) findViewById(R.id.fullScreenAnnexButton);
 
+		// Listener sur la layout entier (juste sur un lien vers une annexe et
+		// la barre du milieu plus tard)
 		layout.setOnTouchListener(new View.OnTouchListener() {
 			@Override
 			public boolean onTouch(View arg0, MotionEvent arg1) {
@@ -47,7 +59,7 @@ public class Annexes extends Activity {
 				case MotionEvent.ACTION_DOWN: // PRESS
 					switch (state) {
 					case NOT_DISPLAYED:
-						setAnnexeX(xmax / 2);
+						setAnnexeXAndX(xmax / 2);
 						state = AnnexesState.DISPLAYED_FREE;
 						break;
 					case DISPLAYED_FREE:
@@ -69,7 +81,9 @@ public class Annexes extends Activity {
 					case DISPLAYED_FREE:
 						break;
 					case DISPLAYED_PRESSED:
-						setAnnexeX((int) arg1.getX());
+						if (arg1.getX() >= xmin && arg1.getX() <= xmax - xmin) {
+							setAnnexeXAndX((int) arg1.getX());
+						}
 						state = AnnexesState.DISPLAYED_PRESSED;
 						break;
 					case DISPLAYED_FULLSCREEN:
@@ -91,7 +105,53 @@ public class Annexes extends Activity {
 					}
 					break;
 				}
-				return true;
+				return false;
+			}
+		});
+
+		// Listener sur le bouton fermer.
+		closeAnnexButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				switch (state) {
+				case NOT_DISPLAYED:
+					break;
+				case DISPLAYED_FREE:
+					setAnnexeX(xmax);
+					state = AnnexesState.NOT_DISPLAYED;
+					break;
+				case DISPLAYED_PRESSED:
+					break;
+				case DISPLAYED_FULLSCREEN:
+					setAnnexeX(xmax);
+					state = AnnexesState.NOT_DISPLAYED;
+					break;
+				}
+			}
+		});
+
+		// Listener sur le bouton plein écran.
+		fullScreenAnnexButton.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				switch (state) {
+				case NOT_DISPLAYED:
+					break;
+				case DISPLAYED_FREE:
+					setAnnexeX(0);
+					fullScreenAnnexButton.setText("Window");
+					state = AnnexesState.DISPLAYED_FULLSCREEN;
+					break;
+				case DISPLAYED_PRESSED:
+					break;
+				case DISPLAYED_FULLSCREEN:
+					setAnnexeX(x);
+					fullScreenAnnexButton.setText("FullScreen");
+					state = AnnexesState.DISPLAYED_FREE;
+					break;
+				}
 			}
 		});
 
