0788af1ef059bdedc071c9c3bb46f41c81fc5410
Gui <Gui@Gui-PCport.mshome.net>
2014-02-19 20:31:31 +0100

    AnnexesState : Ajout des commentaires sur les états des annexes

--- Nomad/src/com/eads/co/nomad/AnnexesState.java
+++ Nomad/src/com/eads/co/nomad/AnnexesState.java
@@ -1,5 +1,10 @@
 package com.eads.co.nomad;
 
+//NOT_DISPLAYED : Annexe pas affichée
+//DISPLAYED_FREE : Annexe affichée mais aucune action dessus
+//DISPLAYED_PRESSED : Annexe affichée et action sur la barre
+//DISPLAYED_FULLSCREEN : Annexe en plein écran
+
 public enum AnnexesState {
 	NOT_DISPLAYED, DISPLAYED_FREE, DISPLAYED_PRESSED, DISPLAYED_FULLSCREEN
 }
