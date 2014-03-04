22955246ec39b0cc9b1b5869bcbe2f51d30bb2e3
benjaminLouradour <benjamin.louradour@gmail.com>
2014-03-04 22:25:20 +0100

    [Recherche]
    le retour se fait sur le menu et plus sur MainActivity

--- Nomad/src/com/eads/co/nomad/Research.java
+++ Nomad/src/com/eads/co/nomad/Research.java
@@ -34,7 +34,7 @@ public class Research extends Activity {
     public boolean onOptionsItemSelected(MenuItem item){       
 		switch (item.getItemId()) {
 			case android.R.id.home:
-				Intent intent = new Intent(this, MainActivity.class);
+				Intent intent = new Intent(this, MenuApp.class);
 				intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 				startActivity(intent);
 				return true;
